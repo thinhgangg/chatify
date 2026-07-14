d05a363a4946c2da4c65bdf020f8782c5c9b18a6
Phuc Thinh <phucthinhmai00@gmail.com>
2026-07-14 22:34:24 +0700

    refactor: remove commented log statement for online user connection

--- backend/src/socket/index.js
+++ backend/src/socket/index.js
@@ -22,8 +22,6 @@ const onlineUsers = new Map(); // {userId: socketId}
 io.on("connection", async (socket) => {
   const user = socket.user;
 
-  // console.log(`${user.displayName} online: ${socket.id}`);
-
   onlineUsers.set(user._id, socket.id);
 
   io.emit("online-users", Array.from(onlineUsers.keys()));
