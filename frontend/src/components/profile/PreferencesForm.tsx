13723d3d69ac7e2415df2e8e516511005ece372a
Mai Phuc Thinh <140177041+thinhgangg@users.noreply.github.com>
2026-07-25 21:48:07 +0700

    Fix comment typo in PreferencesForm.tsx

--- frontend/src/components/profile/PreferencesForm.tsx
+++ frontend/src/components/profile/PreferencesForm.tsx
@@ -14,7 +14,7 @@ import { useState } from "react";
 const PreferencesForm = () => {
   const { isDark, toggleTheme } = useThemeStore();
 
-  //   Handle logic setOnlineStatus
+  //   handle logic setOnlineStatus
   const [onlineStatus, setOnlineStatus] = useState(false);
 
   return (
